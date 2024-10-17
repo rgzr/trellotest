bd2df43f29bfdf328aa714f6fa6522fa07076459
Roger Zaragoza <roger.zr@gmail.com>
2024-10-17 09:52:41 +0200

    Add and quantity

--- js/client.js
+++ js/client.js
@@ -1,16 +1,14 @@
 var GRAY_ICON = './images/icon-gray.svg';
 
 var cardButtonCallback = function(t) {
-  // Show a popup when the button is clicked
-  return t.popup({
+  return t.modal({
     title: 'Gestiona materials',
-    url: 'product-manager.html',  // The popup points to an HTML page
-    height: 250
+    url: 'product-manager.html', 
+    fullscreen: true,
   });
 }
 
 TrelloPowerUp.initialize({
-  // Define card buttons
   'card-buttons': function(t, options) {
     return [{
       icon: GRAY_ICON,
